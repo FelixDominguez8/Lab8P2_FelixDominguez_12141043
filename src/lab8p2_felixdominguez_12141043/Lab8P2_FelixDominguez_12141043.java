8b37d8020dd57bdc3c22967e20e9121ebd8a85b2
Felix Dominguez <66543823+StickyFingers84@users.noreply.github.com>
2022-03-11 13:29:38 -0600

    Primer Commit
    Inicio

---
+++ src/lab8p2_felixdominguez_12141043/Lab8P2_FelixDominguez_12141043.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab8p2_felixdominguez_12141043;
+
+/**
+ *
+ * @author Lenovo I7
+ */
+public class Lab8P2_FelixDominguez_12141043 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
